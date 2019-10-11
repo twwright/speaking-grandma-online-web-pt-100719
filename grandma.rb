111ad25f59d8868653026b8cc97692362af583ea
Thomas Wright <twwright@gmail.com>
2019-10-11 02:21:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 
 def speak_to_grandma(smalltalk)
-if smalltalk == smalltalk.upcase 
+if smalltalk == smalltalk.upcase
   puts "NO, NOT SINCE 1938!"
 if smalltalk == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
-else 
+else
   "HUH?! SPEAK UP, SONY!"
 end
 
@@ -16,7 +16,7 @@ end
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
