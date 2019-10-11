e28e04e88872da9055f983c5afd48ea89d0a158c
Thomas Wright <twwright@gmail.com>
2019-10-11 02:27:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(smalltalk)
   if smalltalk == smalltalk.upcase
-    print "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
   elsif smalltalk == "I LOVE YOU GRANDMA!"
-    print "I LOVE YOU TOO PUMPKIN!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
